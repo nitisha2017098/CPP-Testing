b9c4f719b330d369d2a468e2511a40adbc07b72b
nitisha2017098 <nitisingh8863@gmail.com>
2023-05-06 01:44:33 +0530

    sort array added!

---
+++ sortArray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// sorting an array of 0's , 1's and 2's 
+
+void sortArray(int arr[], int n){
+
+
+}
+
+
+
+
+
+int main(){
+
+    int arr[] = {2,3,5,7,12,43,65,23};
+
+
+    return 0;
+}
