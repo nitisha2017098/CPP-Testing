f2890f394c93112948121c2077c820a5c7e19a30
nitisha2017098 <nitisingh8863@gmail.com>
2023-05-10 14:49:28 +0530

    changes done

--- sortArray.cpp
+++ sortArray.cpp
@@ -4,6 +4,35 @@ using namespace std;
 // sorting an array of 0's , 1's and 2's 
 
 void sortArray(int arr[], int n){
+    int one = 0, two = 0, zero = 0;
+
+    // count all number
+    for (int i=0;i<n; i++){
+        if(arr[i] == 0)
+            zero++;
+        else if(arr[i] == 1)
+            one++;
+        else
+            two++;
+    }
+
+    int i = 0;
+    // put zero
+    while(zero--){
+        arr[i] == 0;
+        i++;
+    }
+
+    // put one
+    while(one--){
+        arr[i] == 1;
+        i++;
+    }
+
+    while(two--){
+        arr[i] == 2;
+        i++;
+    }
 
 
 }
@@ -14,7 +43,16 @@ void sortArray(int arr[], int n){
 
 int main(){
 
-    int arr[] = {2,3,5,7,12,43,65,23};
+    int arr[] = {0,1,2,2,1,0,0,1,2,2,1};
+    sortArray(arr,11);
+
+    // print array
+    for (int i=0;i<11;i++){
+        cout << arr[i] << " ";
+
+    }
+
+    cout << endl;
 
 
     return 0;
